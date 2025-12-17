35eafdadcdcea8e29da9e5ac2178f1aab7e0a41b
Walter Cordova <wcordova@beesion.com>
2025-12-17 12:35:46 -0600

    feat: hello_word

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hola mundo!'),
+        ),
+      )
+    );
+  }
+  
+}
